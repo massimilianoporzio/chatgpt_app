eb974580b2b89e5bbbcb6e3c17fad3b36281a3b3
MASSIMILIANO PORZIO <massimiliano.porzio@gmail.com>
2023-03-21 21:20:05 +0100

    my app impostata

--- lib/app/my_app.dart
+++ lib/app/my_app.dart
@@ -1,14 +1,18 @@
+import 'package:chatgpt_app/app/routes/on_genereate_route.dart';
 import 'package:flutter/material.dart';
+import 'package:loggy/loggy.dart';
 
-class Myapp extends StatelessWidget {
-  const Myapp({super.key});
+class MyApp extends StatelessWidget with UiLoggy {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
+    loggy.debug("Material App creation");
     return MaterialApp(
       title: 'Material App',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(brightness: Brightness.dark),
+      onGenerateRoute: OnGenerateRoute.route,
       initialRoute: '/',
     );
   }
